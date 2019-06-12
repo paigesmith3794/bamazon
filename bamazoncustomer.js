16c9bead527f473b31f70c5998a154c312d91886
paigesmith3794 <paigesmith3794@gmail.com>
2019-06-11 21:47:04 -0700

    sql connection giving me problems

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 8889,
+    host: "127.0.0.1",
+    port: 3306,
     user: "root",
-    password: "Tommysb1",
+    password: "root",
     database: "bamazon"
 });
 
